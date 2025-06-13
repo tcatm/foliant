212d6daf82067a83d8ae84f9e3ac789c5eb43bd2
Nils Schneider <nils.schneider@posteo.de>
2025-06-14 01:16:26 +0200

    remove bitmap_filter

--- src/multi_list/mod.rs
+++ src/multi_list/mod.rs
@@ -1,11 +1,9 @@
 //! Modular multi-shard listing with lazy bitmap-based filtering.
 
-mod bitmap_filter;
 mod streamer;
 mod lazy_filter;
 mod lazy_tag_filter;
 
-pub use bitmap_filter::ShardBitmapFilter;
 pub use streamer::MultiShardListStreamer;
 pub use lazy_filter::{LazyShardFilter, ComposedFilter, IdentityFilter, FilterComposition};
 pub use lazy_tag_filter::{LazyTagFilter, TagFilterConfig};
